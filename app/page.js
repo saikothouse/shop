03d8aec1a7e2810d818a568ac4b7290d429fbe2f
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-04 23:39:33 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import ProductList from '../components/ProductList';
 const Home = () => {
     return (
         <div className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-4">Welcome to My E-Commerce Site</h1>
+            <h1 className="text-3xl font-bold mb-4">Nullbite</h1>
             <ProductList />
         </div>
     );
